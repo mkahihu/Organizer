ddb4820a27165ef70cd32f9e9591f27d30dc2239
alvarado17c <alvarado17c@ncssm.edu>
2016-05-12 14:16:36 -0400

    Added a file from StackOverflow we might be able to use for ranking system. Dabbled with Organizer.java

---
+++ RankingList.java
@@ -0,0 +1,36 @@
+public class RankingList implements Comparable<RankingList> {
+    private final String ranking;
+    private final int score;
+
+    public RankingList(String ranking, int score) {
+        this.ranking = ranking;
+        this.score = score;
+    }
+
+    public String ranking() {
+        return ranking;
+    }
+
+    public int score() {
+        return score;
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof RankingList))
+            return false;
+        RankingList rankingList = (RankingList) o;
+        return rankingList.ranking.equals(ranking) && rankingList.score == score;
+    }
+
+    // public int hashCode() {
+    //    return your hash;
+    // }
+
+    public String toString(){
+        return "Rank: " + ranking +", points = " + score ;
+    }
+
+    public int compareTo(RankingList rankingList) {
+        return score > rankingList.score ? 1 : score < rankingList.score ? -1 : 0;
+    }
+}
